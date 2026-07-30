304d4d667208d2216a16ebf89caa2ecc08165a4c
marko-obradovic <marko@obradovic.co.uk>
2026-07-30 16:28:10 +0100

    AdWrite program to count how many numbers between 1 and 100 are divisible by 3 with no remainder

---
+++ basics/DisplayDivisibleNumbersInRange/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace DisplayDivisibleNumbersInRange
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int lowerRange = 1;
+      int upperRange = 100;
+
+      for (int i = lowerRange+1; i < upperRange; i++)
+      {
+        if (i % 3 == 0)
+        {
+          Console.WriteLine(i);
+        }
+      }
+    }
+  }
+}
