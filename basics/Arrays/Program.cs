04954f5923f09585282c970b0a0a06a2223c2b10
marko-obradovic <marko@obradovic.co.uk>
2026-08-11 18:53:39 +0100

    Finish FacebookLikes exercise

---
+++ basics/Arrays/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Arrays
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var numbers = new int[] {3, 7, 9, 2, 14, 6};
+
+      // Length()
+      Console.WriteLine("Length()");
+      Console.WriteLine("Length: {0}", numbers.Length);
+      Console.WriteLine("Original values");
+      printValues(numbers);
+
+      Console.WriteLine("\nIndexOf()");
+      var numToFind = 9;
+
+      var indexValue = Array.IndexOf(numbers, numToFind);
+      Console.WriteLine("index of {0}: {1}", numToFind, indexValue);
+
+      Console.WriteLine("\nClear()");
+      Array.Clear(numbers, 0, 2);
+
+      printValues(numbers);
+
+      Console.WriteLine("\nCopy()");
+      var newNumbers = new int[3];
+
+      Array.Copy(numbers, 2, newNumbers, 0, 3);
+
+      printValues(newNumbers);
+
+      Console.WriteLine("\nSort()");
+      Array.Sort(numbers);
+      printValues(numbers);
+
+      Console.WriteLine("\nReverse()");
+      Array.Reverse(numbers);
+      printValues(numbers);
+    }
+
+    static void printValues(int[] array)
+    {
+      for (int i = 0; i < array.Length; i++)
+      {
+        Console.WriteLine(array[i]);
+      }
+    }
+  }
+}
---
+++ basics/FacebookLikes/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace FacebookLikes
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var names = new List<string>();
+      Console.WriteLine("Enter a name:");
+
+      while (true)
+      {
+        string name = Console.ReadLine();
+
+        if (name == "")
+        {
+          PrintLikesMessage(names);
+          return;
+        }
+
+        names.Add(name);
+      }
+    }
+
+    static void PrintLikesMessage(List<string> names)
+    {
+      if (names.Count >= 3)
+      {
+        Console.WriteLine("{0}, {1}, and {2} others like your post.", names[0], names[1], names.Count - 2);
+        return;
+      }
+
+      if (names.Count == 2)
+      {
+        Console.WriteLine("{0} and {1} like your post.", names[0], names[1]);
+        return;
+      }
+
+      Console.WriteLine("{0} likes your post.", names[0]);
+
+    }
+  }
+}
